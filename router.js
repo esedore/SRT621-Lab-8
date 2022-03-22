8a4bfae489ab3a91bc9406926406a8a6a015e09b
DPI910Group5 <dpi910_group5@protonmail.com>
2022-03-22 16:29:58 -0400

    express route

--- router.js
+++ router.js
@@ -1,3 +1,29 @@
-const routeMap = new RouteMap {
-    "/"
-}
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static("views"));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'views/index.html'));
+})
+app.get('/books',function(req,res){
+    res.sendFile(path.join(__dirname,'views/book-list.html'));
+})
+app.get('/book1',function(req,res){
+    res.sendFile(path.join(__dirname,'views/persepolis.html'));
+})
+app.get('/book2',function(req,res){
+    res.sendFile(path.join(__dirname,'views/shanameh.html'));
+})
+app.get('/book3',function(req,res){
+    res.sendFile(path.join(__dirname,'views/blind_owl.html'));
+})
+app.get('/home',function(req,res){
+    res.sendFile(path.join(__dirname,'views/index.html'));
+})
+
+app.use('/',router);
+app.listen(process.env.port || 3000);
+console.log('Port 3000 listening on');

4bb8dfdbd941432949eab2a937ea6b70e8000a4a
DPI910Group5 <dpi910_group5@protonmail.com>
2022-03-22 17:23:02 -0400

    Update router.js

--- router.js
+++ router.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const router = express.Router();
 
 app.use(express.static("views"));
 app.use(express.static("public"));
 
-app.get('/',function(req,res){
+router.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'views/index.html'));
 })
-app.get('/books',function(req,res){
+router.get('/books',function(req,res){
     res.sendFile(path.join(__dirname,'views/book-list.html'));
 })
-app.get('/book1',function(req,res){
+router.get('/book1',function(req,res){
     res.sendFile(path.join(__dirname,'views/persepolis.html'));
 })
-app.get('/book2',function(req,res){
+router.get('/book2',function(req,res){
     res.sendFile(path.join(__dirname,'views/shanameh.html'));
 })
-app.get('/book3',function(req,res){
+router.get('/book3',function(req,res){
     res.sendFile(path.join(__dirname,'views/blind_owl.html'));
 })
-app.get('/home',function(req,res){
+router.get('/home',function(req,res){
     res.sendFile(path.join(__dirname,'views/index.html'));
 })
 module.exports = router;

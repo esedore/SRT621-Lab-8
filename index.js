24ba2f63010c994681c25825755b00156b2613ba
DPI910Group5 <dpi910_group5@protonmail.com>
2022-03-22 17:20:43 -0400

    update index

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+const routes = require('router.js');
+const express = require('express');
+const path = require('./router');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'html'));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.set('view engine', 'ejs');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.use('/',routes);
+
+app.listen(3000, () => {
+    console.log('listening on port 3K');
 });
--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = express.Router();
+
 
 app.use(express.static("views"));
 app.use(express.static("public"));
@@ -24,6 +24,7 @@ app.get('/book3',function(req,res){
 app.get('/home',function(req,res){
     res.sendFile(path.join(__dirname,'views/index.html'));
 })
+module.exports = router;
 
 app.use('/',router);
 app.listen(process.env.port || 3000);
